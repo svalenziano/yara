fe041e68d586fc8800c37b39d3e8d5af2a064169
SValenziano <senorvalenz@gmail.com>
2026-03-31 09:04:41 -0400

    start routing

--- src/yara/cli/chat_ui.py
+++ src/yara/cli/chat_ui.py
@@ -6,7 +6,7 @@ from prompt_toolkit.formatted_text import HTML
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.cursor_shapes import CursorShape
 
-from yara.services.query import query_similar_chunks_pretty
+from yara.services.get_chunks import query_similar_chunks_pretty
 from yara.services.openai_client import client
 
 
@@ -55,10 +55,22 @@ def chat_loop():
 
         found = query_similar_chunks_pretty(ask)
 
+        """
+        Todo: classifier
+
+        IDEAS
+            1) 
+                - execute classifer, return a 'route' string
+                - switch modes based on that string
+
+            2) 
+                - ???
+        """
+
         conversation.append({
             "role": "user",
             "content": f"""Please use these documents to answer my question.
-                Please do NOT rely on your training to answer my question.
+                Please do NOT rely on your training knowledge to answer my question.
                 If the question is not answerable based on these documents, please let me know.
 
                 Here are the documents:
